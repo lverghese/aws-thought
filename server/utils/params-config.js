cea75bf166b6455aa2edab7cc5ec14c6cc09f7c0
lverghese <loganverghese@gmail.com>
2021-12-04 01:10:06 -0500

    working on image upload route

---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+
+    const imageParams = {
+        Bucket: "user-images-466d7df8-4689-4f42-9c12-065f7477a287",
+        Key: `${uuidv4()}.${fileType}`,
+        Body: fileName.buffer
+    };
+
+    return imageParams;
+};
+
+module.exports = params;
